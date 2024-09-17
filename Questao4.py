dde0168da3094c85f75d4fd33516b0bb6043bbb1
Natalia de Cássia Souza <souzacassianatalia@gmail.com>
2024-09-17 17:36:39 -0300

    ComentariosQuestao4.py

--- Questao4.py
+++ Questao4.py
@@ -1,3 +1,19 @@
+#4) Dado o valor de faturamento mensal de uma distribuidora, detalhado por estado:
+#• SP – R$67.836,43
+#• RJ – R$36.678,66
+#• MG – R$29.229,88
+#• ES – R$27.165,48
+#• Outros – R$19.849,53
+
+#Escreva um programa na linguagem que desejar onde calcule o percentual de representação que cada estado teve dentro do valor total mensal da distribuidora.  
+
+
+
+
+
+
+
+
 import re
 lista_faturamento = ["SP – r$67836.43","RJ – r$36678.66","MG – r$29229.88","ES – r$27165.48","OUTROS – r$19849.53"]
 lista_valores=[]
