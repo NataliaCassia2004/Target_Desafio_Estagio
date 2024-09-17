1d6b271199bd4d2ca950b17d7d35bf8bce6c0715
Natalia de Cássia Souza <souzacassianatalia@gmail.com>
2024-09-17 17:35:19 -0300

    ComentarioQuestao1.py

--- Questao1.py
+++ Questao1.py
@@ -1,3 +1,9 @@
+#1) Observe o trecho de código abaixo: int INDICE = 13, SOMA = 0, K = 0;
+#Enquanto K < INDICE faça { K = K + 1; SOMA = SOMA + K; }
+#Imprimir(SOMA);
+#Ao final do processamento, qual será o valor da variável SOMA?
+
+
 indice = 13
 soma = 0
 k = 0
