f274a9088dcfd71c727c6b4b7f1752cd404adb21
Natalia de Cássia Souza <souzacassianatalia@gmail.com>
2024-09-17 17:36:18 -0300

    Comentarios Questao3.py

--- Questao3.py
+++ Questao3.py
@@ -1,4 +1,8 @@
 # não encontrei o arquivo json, então criei um com 30 valores de faturamento
+#3) Dado um vetor que guarda o valor de faturamento diário de uma distribuidora, faça um programa, na linguagem que desejar, que calcule e retorne:
+#• O menor valor de faturamento ocorrido em um dia do mês;
+#• O maior valor de faturamento ocorrido em um dia do mês;
+#• Número de dias no mês em que o valor de faturamento diário foi superior à média mensal.
 
 import json
 
